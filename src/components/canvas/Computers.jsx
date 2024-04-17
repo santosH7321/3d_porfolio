1e5c0995e118f40d404dba3869948ee2a820bbcc
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-04-17 07:27:04 +0530

    cardSection

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,13 +11,14 @@ const Computers = ({ isMobile }) => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
-        position={[-20, 50, 10]}
+        position= {[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
         castShadow
-        shadow-mapSize={1024}
+        shadowMapSize={{ width: 1024, height: 1024 }}
       />
+
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
